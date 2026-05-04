eea55eb69cf19df980e81db9e5bc3e6428c7d9e2
Paul TO <kto@ebi.ac.uk>
2026-05-04 03:41:55 +0100

    Updated search entities annotation, removed search entities jq postprocessing

--- src/open_targets_platform_mcp/model/search_entities_result.py
+++ src/open_targets_platform_mcp/model/search_entities_result.py
@@ -1,10 +1,6 @@
-from pydantic import BaseModel, Field
-
-from open_targets_platform_mcp.model.query_result import BatchQueryResult
+from pydantic import BaseModel
 
 
-class SearchEntitiesResult(BaseModel):
-    batch_query_result: BatchQueryResult = Field(
-        ...,
-        description="The status of the query result.",
-    )
+class SearchEntitiesFoundEntity(BaseModel):
+    id: str
+    type: str
--- src/open_targets_platform_mcp/tools/search_entities/search_entities.py
+++ src/open_targets_platform_mcp/tools/search_entities/search_entities.py
@@ -5,7 +5,7 @@ from typing import Annotated
 from pydantic import Field
 
 from open_targets_platform_mcp.model.query_result import QueryResultStatus
-from open_targets_platform_mcp.model.search_entities_result import SearchEntitiesResultHit
+from open_targets_platform_mcp.model.search_entities_result import SearchEntitiesFoundEntity
 from open_targets_platform_mcp.tools.batch_query.batch_query import batch_query_with_jq
 
 VARIABLE_FIELD = "queryString"
@@ -26,16 +26,13 @@ async def search_entities(
     query_strings: Annotated[
         list[str],
         Field(
-            description="List of search queries",
-            examples=[
-                ["BRCA1"],
-                ["breast cancer", "aspirin"],
-            ],
+            description="List of search queries.",
+            examples=[["BRCA1", "aspirin"]],
         ),
     ],
 ) -> Annotated[
-    dict[str, list[SearchEntitiesResultHit]],
-    Field(description="Search entities result"),
+    dict[str, list[SearchEntitiesFoundEntity]],
+    "Top 3 hits for each query string, with entity ID and type.",
 ]:
     batch_query_result = await batch_query_with_jq(
         SEARCH_ENTITY_QUERY,
@@ -43,15 +40,15 @@ async def search_entities(
         VARIABLE_FIELD,
     )
 
-    result = dict[str, list[SearchEntitiesResultHit]]()
+    result = dict[str, list[SearchEntitiesFoundEntity]]()
     for query_result in batch_query_result.results:
         if query_result.id is None:
             continue
         query_string = query_result.id
-        entities = list[SearchEntitiesResultHit]()
+        entities = list[SearchEntitiesFoundEntity]()
         if query_result.result.status == QueryResultStatus.SUCCESS and query_result.result.data is not None:
-            for hit in query_result.result.data.get("search", {}).get("hits", []):
-                entities.append(SearchEntitiesResultHit(entity_id=hit["id"], entity_type=hit["entity"]))
+            for hit in query_result.result.data.get("search", {}).get("hits", [])[:3]:
+                entities.append(SearchEntitiesFoundEntity(id=hit["id"], type=hit["entity"]))
         result[query_string] = entities
 
     return result
